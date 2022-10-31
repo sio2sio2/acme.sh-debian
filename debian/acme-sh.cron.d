e87f707fef25c72bbf9cb03d40bb055472c8d706
Benjamin Sonntag <benjamin@octopuce.fr>
2022-10-31 11:47:21 +0100

    [fix] fixing cron issue

--- debian/acme-sh.cron.d
+++ debian/acme-sh.cron.d
@@ -1,4 +1,6 @@
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # Renew all certificates obtained by root and located in /etc/acme.sh 
-0 15 * * * /usr/bin/acme.sh --cron --home "/etc/acme.sh/" > /dev/null
+0 15 * * *   root  /usr/bin/acme.sh --cron --home "/etc/acme.sh/" > /dev/null
 
